553eeaa0327eeaad8b92157bb4965415df1d055a
Thiago Guimarães Pizetta <thiagopizetta12@hotmail.com>
2020-07-23 19:35:06 -0300

    corrigir duplicação de produtos no carrinho

--- PooLojaVirtual.Tests/CarrinhoTests.cs
+++ PooLojaVirtual.Tests/CarrinhoTests.cs
@@ -75,5 +75,25 @@ namespace PooLojaVirtual.Tests
             carrinho.Remover(produto.Id);
             Assert.Empty(carrinho.Itens);
         }
+
+        [Fact]
+        public void Carrinho_Nao_Deve_Duplicar_Produtos()
+        {
+            // preparar
+            var carrinho = new Carrinho();
+            var produto = new Produto
+            {
+               Id = 1,
+               Nome = "Smartphone",
+               Preco = 799 
+            };
+            // executar
+            carrinho.Adicionar(produto, 1);
+            carrinho.Adicionar(produto, 1);
+
+            // verificar 
+            Assert.Equal(1, carrinho.Itens.Count());
+            Assert.Equal(1598, carrinho.Total);
+        }
     }
 }
